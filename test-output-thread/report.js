6e469955fe83180db2f10bf0e42066aa4fb86edc
Pratim <pratimtester09@gmail.com>
2022-11-06 23:51:41 +0530

    Updated Selenium Hybrid framework with POM BDD and TestNG extent reports project

---
+++ test-output-thread/report.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "f37f519b-cca4-4ab9-b99f-ca533c3022ec",
+    "feature": "Gym details feature",
+    "scenario": "As a gym owner once the details are added now i should only see the edit button",
+    "start": 1667712284243,
+    "group": 14,
+    "content": "",
+    "tags": "",
+    "end": 1667712291670,
+    "className": "failed"
+  },
+  {
+    "id": "27147707-b279-4685-aedb-664ea961ed50",
+    "feature": "Gym details feature",
+    "scenario": "As a gym owner he/she should be able to add the gym details for first time",
+    "start": 1667712254603,
+    "group": 14,
+    "content": "",
+    "tags": "",
+    "end": 1667712277167,
+    "className": "undefined"
+  },
+  {
+    "id": "1ff26565-3987-4ea4-bca8-0ac7932534f0",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1667712314804,
+    "group": 14,
+    "content": "",
+    "tags": "",
+    "end": 1667712327225,
+    "className": "pending"
+  },
+  {
+    "id": "9cdf7622-429b-46a4-94b5-6479ae6c5a9c",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1667712305290,
+    "group": 14,
+    "content": "",
+    "tags": "",
+    "end": 1667712314793,
+    "className": "failed"
+  },
+  {
+    "id": "619a5647-109d-447e-98fd-33b152e4915a",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1667712291706,
+    "group": 14,
+    "content": "",
+    "tags": "",
+    "end": 1667712305282,
+    "className": "passed"
+  },
+  {
+    "id": "414fc7a1-6416-49e0-a35f-17a1ebf0a09d",
+    "feature": "Dashboard feature",
+    "scenario": "Dashboard page title",
+    "start": 1667712216358,
+    "group": 14,
+    "content": "",
+    "tags": "",
+    "end": 1667712254448,
+    "className": "passed"
+  },
+  {
+    "id": "dbf3590b-8625-4ca6-afb0-5ca72c7bb681",
+    "feature": "Gym details feature",
+    "scenario": "Gym profile picture can be added but this not mandatory",
+    "start": 1667712277221,
+    "group": 14,
+    "content": "",
+    "tags": "",
+    "end": 1667712284175,
+    "className": "failed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 14,
+    "content": "Thread[TestNG-test\u003dSurefire test-1,5,main]"
+  }
+]);
+});
